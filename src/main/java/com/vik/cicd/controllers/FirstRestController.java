691dcc0bb42046bef83655d127083d6876f71dcd
Vikash Singh <springtestabc111@gmail.com>
2023-05-21 11:08:48 +0530

    initial commit

---
+++ src/main/java/com/vik/cicd/controllers/FirstRestController.java
@@ -0,0 +1,21 @@
+package com.vik.cicd.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstRestController {
+
+    @GetMapping("welcome")
+    public String welcome(){
+
+        return "Welcome to first controller";
+    }
+
+   /* git init
+    git add README.md
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/vikashspring/github-cicd-action.git
+    git push -u origin main*/
+}
---
+++ src/test/java/com/vik/cicd/GithubCicdActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vik.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
